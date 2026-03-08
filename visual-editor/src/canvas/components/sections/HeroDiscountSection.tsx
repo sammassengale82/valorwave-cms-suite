1465929f731f4796ac7351b54cd23821b03f4630
Samuel Massengale <valorwaveentertainment@gmail.com>
2026-03-08 16:55:51 -0400

    Update HeroDiscountSection.tsx

--- visual-editor/src/canvas/components/sections/HeroDiscountSection.tsx
+++ visual-editor/src/canvas/components/sections/HeroDiscountSection.tsx
@@ -1,28 +1,17 @@
 import React from "react";
 
-interface Props {
-  heading?: string;
-  subheading?: string;
-  text1?: string;
-  text2?: string;
-}
-
 export default function HeroDiscountSection({
   heading,
   subheading,
   text1,
   text2
-}: Props) {
+}) {
   return (
     <section className="hero-discount-section">
       {heading && <h2>{heading}</h2>}
       {subheading && <h3>{subheading}</h3>}
-      {text1 && (
-        <p dangerouslySetInnerHTML={{ __html: text1 }} />
-      )}
-      {text2 && (
-        <p dangerouslySetInnerHTML={{ __html: text2 }} />
-      )}
+      {text1 && <p dangerouslySetInnerHTML={{ __html: text1 }} />}
+      {text2 && <p dangerouslySetInnerHTML={{ __html: text2 }} />}
     </section>
   );
 }
