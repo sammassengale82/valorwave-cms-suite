c1ae2300aa8fc2abd7202fb6de9a3c0346032abe
Samuel Massengale <valorwaveentertainment@gmail.com>
2026-03-08 16:55:34 -0400

    Update BrandMeaningSection.tsx

--- visual-editor/src/canvas/components/sections/BrandMeaningSection.tsx
+++ visual-editor/src/canvas/components/sections/BrandMeaningSection.tsx
@@ -1,30 +1,12 @@
 import React from "react";
 
-interface Props {
-  heading?: string;
-  p1?: string;
-  p2?: string;
-  p3?: string;
-}
-
-export default function BrandMeaningSection({
-  heading,
-  p1,
-  p2,
-  p3
-}: Props) {
+export default function BrandMeaningSection({ heading, p1, p2, p3 }) {
   return (
     <section className="brand-meaning-section">
       {heading && <h2>{heading}</h2>}
-      {p1 && (
-        <p dangerouslySetInnerHTML={{ __html: p1 }} />
-      )}
-      {p2 && (
-        <p dangerouslySetInnerHTML={{ __html: p2 }} />
-      )}
-      {p3 && (
-        <p dangerouslySetInnerHTML={{ __html: p3 }} />
-      )}
+      {p1 && <p dangerouslySetInnerHTML={{ __html: p1 }} />}
+      {p2 && <p dangerouslySetInnerHTML={{ __html: p2 }} />}
+      {p3 && <p dangerouslySetInnerHTML={{ __html: p3 }} />}
     </section>
   );
 }
