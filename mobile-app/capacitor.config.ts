631dd2342d5259f28fe1f954cab65e1f6c6531a1
Samuel Massengale <valorwaveentertainment@gmail.com>
2026-03-08 01:38:25 -0500

    Create capacitor.config.ts

---
+++ mobile-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/core';
+
+const config: CapacitorConfig = {
+  appId: 'com.valorwave.cms',
+  appName: 'ValorWave CMS',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
