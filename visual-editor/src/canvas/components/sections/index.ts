a6e06987e62fcf1cc86c06c026a485c5bf268c6b
Samuel Massengale <valorwaveentertainment@gmail.com>
2026-03-08 16:32:19 -0400

    Create sections/index.ts

---
+++ visual-editor/src/canvas/components/sections/index.ts
@@ -0,0 +1,35 @@
+import HeroSection from "./HeroSection";
+import NavigationSection from "./NavigationSection";
+import HeaderSection from "./HeaderSection";
+import ServicesSection from "./ServicesSection";
+import ServiceAreaSection from "./ServiceAreaSection";
+import BioSection from "./BioSection";
+import WeddingDJSection from "./WeddingDJSection";
+import FAQSection from "./FAQSection";
+import BrandMeaningSection from "./BrandMeaningSection";
+import HeroDiscountSection from "./HeroDiscountSection";
+import CalendarSection from "./CalendarSection";
+import TestimonialFormSection from "./TestimonialFormSection";
+import TestimonialsSection from "./TestimonialsSection";
+import FooterSection from "./FooterSection";
+import SEOSection from "./SEOSection";
+import GoogleSection from "./GoogleSection";
+
+export const SectionComponents: Record<string, any> = {
+  HeroSection,
+  NavigationSection,
+  HeaderSection,
+  ServicesSection,
+  ServiceAreaSection,
+  BioSection,
+  WeddingDJSection,
+  FAQSection,
+  BrandMeaningSection,
+  HeroDiscountSection,
+  CalendarSection,
+  TestimonialFormSection,
+  TestimonialsSection,
+  FooterSection,
+  SEOSection,
+  GoogleSection
+};
