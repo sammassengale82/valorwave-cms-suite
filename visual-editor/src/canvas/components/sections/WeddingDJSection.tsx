e6fb850fd64a2ad122b1bb541a158e6dac4f30fd
Samuel Massengale <valorwaveentertainment@gmail.com>
2026-03-08 16:54:50 -0400

    Update WeddingDJSection.tsx

--- visual-editor/src/canvas/components/sections/WeddingDJSection.tsx
+++ visual-editor/src/canvas/components/sections/WeddingDJSection.tsx
@@ -1,16 +1,5 @@
 import React from "react";
 
-interface Props {
-  heading?: string;
-  intro?: string;
-  c1title?: string;
-  c1text?: string;
-  c2title?: string;
-  c2text?: string;
-  c3title?: string;
-  c3text?: string;
-}
-
 export default function WeddingDJSection({
   heading,
   intro,
@@ -20,34 +9,25 @@ export default function WeddingDJSection({
   c2text,
   c3title,
   c3text
-}: Props) {
+}) {
   return (
     <section className="wedding-dj-section">
       {heading && <h2>{heading}</h2>}
       {intro && (
-        <p
-          className="wedding-dj-intro"
-          dangerouslySetInnerHTML={{ __html: intro }}
-        />
+        <p dangerouslySetInnerHTML={{ __html: intro }} />
       )}
       <div className="wedding-dj-cards">
         <div className="wedding-dj-card">
           {c1title && <h3>{c1title}</h3>}
-          {c1text && (
-            <p dangerouslySetInnerHTML={{ __html: c1text }} />
-          )}
+          {c1text && <p dangerouslySetInnerHTML={{ __html: c1text }} />}
         </div>
         <div className="wedding-dj-card">
           {c2title && <h3>{c2title}</h3>}
-          {c2text && (
-            <p dangerouslySetInnerHTML={{ __html: c2text }} />
-          )}
+          {c2text && <p dangerouslySetInnerHTML={{ __html: c2text }} />}
         </div>
         <div className="wedding-dj-card">
           {c3title && <h3>{c3title}</h3>}
-          {c3text && (
-            <p dangerouslySetInnerHTML={{ __html: c3text }} />
-          )}
+          {c3text && <p dangerouslySetInnerHTML={{ __html: c3text }} />}
         </div>
       </div>
     </section>
