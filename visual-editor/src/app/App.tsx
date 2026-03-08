3d28a267c75b40d08bc4a4c18079eb44c19c408a
Samuel Massengale <valorwaveentertainment@gmail.com>
2026-03-08 14:48:12 -0400

    Update App.tsx

--- visual-editor/src/app/App.tsx
+++ visual-editor/src/app/App.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Sidebar from "../components/Sidebar";
 import Toolbar from "../components/Toolbar";
 import Canvas from "../canvas/Canvas";
 import Inspector from "../inspector/Inspector";
 import CMSPanel from "../cms-panel/CMSPanel";
 import DeviceSwitcher from "../devices/DeviceSwitcher";
+import { getDraft, saveDraft } from "../api/api";
+import { deserializeFromCMS } from "../serialization/deserializeFromCMS";
+import { serializeToCMS } from "../serialization/serializeToCMS";
+import { useCanvasState } from "../canvas/CanvasState";
 
 export default function App() {
+  const setTree = useCanvasState((s) => s.setTree);
+
+  useEffect(() => {
+    (async () => {
+      const cms = await getDraft();
+      const tree = deserializeFromCMS(cms);
+      setTree(tree, false);
+    })();
+  }, [setTree]);
+
+  async function handleSave() {
+    const state = useCanvasState.getState();
+    const cms = await getDraft();
+    const updated = serializeToCMS(state.tree, cms);
+    await saveDraft(updated);
+  }
+
   return (
     <div className="editor-root">
-      <Toolbar />
+      <Toolbar onSave={handleSave} />
       <div className="editor-body">
         <Sidebar />
         <Canvas />
