f07a8a5096d0b47e3d75fc26b6242f60309a3038
Samuel Massengale <valorwaveentertainment@gmail.com>
2026-03-10 03:32:17 -0400

    Update Canvas.tsx

--- components/canvas/Canvas.tsx
+++ components/canvas/Canvas.tsx
@@ -1,13 +1,8 @@
 import React from "react";
-import { RenderTreeNode } from "../../types/renderNode";
 import { RenderNode } from "./RenderNode";
-import { useCanvasState } from "../../hooks/useCanvasState";
+import { useCanvasState } from "../../stores/canvasState";
 
-export interface CanvasProps {
-  initialTree: RenderTreeNode[];
-}
-
-export const Canvas: React.FC<CanvasProps> = ({ initialTree }) => {
+export const Canvas = () => {
   const {
     tree,
     selectedId,
@@ -18,44 +13,9 @@ export const Canvas: React.FC<CanvasProps> = ({ initialTree }) => {
     setDropTarget,
     clearDropTarget,
     insertAt
-  } = useCanvasState(initialTree);
+  } = useCanvasState();
 
   return (
-    <div
-      style={{
-        width: "100%",
-        minHeight: "100%",
-        background: "#e2e8f0",
-        padding: 24,
-        boxSizing: "border-box",
-        overflow: "auto"
-      }}
-    >
-      <div
-        style={{
-          maxWidth: 1200,
-          margin: "0 auto",
-          background: "#ffffff",
-          boxShadow: "0 10px 30px rgba(15, 23, 42, 0.12)"
-        }}
-      >
-        {tree.map((node) => (
-          <RenderNode
-            key={node.id}
-            node={node}
-            selectedId={selectedId}
-            hoveredId={hoveredId}
-            dropTarget={dropTarget}
-            onSelect={setSelectedId}
-            onHover={setHoveredId}
-            onDropZoneEnter={setDropTarget}
-            onDropZoneLeave={clearDropTarget}
-            onDrop={(nodeId, position, data) => {
-              insertAt(nodeId, position, data as RenderTreeNode);
-            }}
-          />
-        ))}
-      </div>
-    </div>
+    <div> ... </div>
   );
 };
