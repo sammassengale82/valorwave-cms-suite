1949ec585be2616059b1a6e99c1c5b930a114bd4
Samuel Massengale <valorwaveentertainment@gmail.com>
2026-03-11 12:17:18 -0400

    Update vite.config.ts

--- visual-editor/src/vite.config.ts
+++ visual-editor/src/vite.config.ts
@@ -3,7 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: ".", // ensure Vite uses THIS folder as root
+  publicDir: "public",
   build: {
-    outDir: "dist"
+    outDir: "dist",
+    emptyOutDir: true
   }
 });
