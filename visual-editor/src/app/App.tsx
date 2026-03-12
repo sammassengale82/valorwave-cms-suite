864b3db43863cb66980c965d7f237bda6b042c02
Samuel Massengale <valorwaveentertainment@gmail.com>
2026-03-12 15:40:25 -0400

    Update App.tsx

--- visual-editor/src/app/App.tsx
+++ visual-editor/src/app/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ThemeProvider } from "../themes/ThemeManager";
 import ThemeSwitcher from "../themes/ThemeSwitcher";
 import PreviewToolbar from "../preview/PreviewToolbar";
@@ -6,7 +6,24 @@ import PreviewContainer from "../preview/PreviewContainer";
 import Canvas from "../canvas/Canvas";
 import InspectorPanel from "../inspector/InspectorPanel";
 
+import { useCanvasState } from "../canvas/CanvasState";
+import { loadTemplateFiles } from "../canvas/templateLoader";
+import { templatesToTree } from "../canvas/templateMapper";
+
 export default function App() {
+  const setTree = useCanvasState((s) => s.setTree);
+
+  useEffect(() => {
+    // 1. Load all template JSON files
+    const templates = loadTemplateFiles();
+
+    // 2. Convert them into editor nodes
+    const tree = templatesToTree(templates);
+
+    // 3. Inject into CanvasState
+    setTree(tree);
+  }, []);
+
   return (
     <ThemeProvider>
       <ThemeSwitcher />
