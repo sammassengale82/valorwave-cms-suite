a0a58e9cde32c5289f0c06c4c2ba6ece464317b0
Samuel Massengale <valorwaveentertainment@gmail.com>
2026-03-13 15:25:06 -0400

    Update main.tsx

--- desktop-app/src/ui/main.tsx
+++ desktop-app/src/ui/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import VisualEditor from "./ui/mount-visual-editor";
+import "./styles/site.css";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(<VisualEditor />);
