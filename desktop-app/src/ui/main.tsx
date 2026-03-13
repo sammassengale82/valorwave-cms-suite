267bb53e9e7a5101d49bb43096b1371514b6b433
sammassengale82 <sammassengale82@gmail.com>
2026-03-13 15:30:57 -0400

    update

--- desktop-app/src/ui/main.tsx
+++ desktop-app/src/ui/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import VisualEditor from "./ui/mount-visual-editor";
-import "./styles/site.css";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(<VisualEditor />);
